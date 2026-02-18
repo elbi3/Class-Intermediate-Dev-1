1f68dbfd283192d77d6d2ba09fabc44457780786
elbi3 <167570227+elbi3@users.noreply.github.com>
2026-02-18 10:46:34 -0800

    Quick Update

---
+++ class-exercises/w3schools.md
@@ -0,0 +1,5 @@
+
+```shell
+python --version
+end()
+```
---
+++ class-exercises/w3schools.py
@@ -0,0 +1,24 @@
+import sys
+
+x = 5
+john = "john"
+jon = 'jon'
+# these are:
+a = 5
+# not the same!
+A = 5
+
+myVar = 'twosday'
+print(type(myVar))
+
+print(sys.version)
+
+# my-var = 5 --> NO!
+
+# 4 spaces indent is common practice
+
+#semicolons optional, rarely used:
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+print("I am a", end=" ")
+print("space rocket!!!")
