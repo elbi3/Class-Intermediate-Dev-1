4b68bc89152515691cfe72239fd4354e6b989a8d
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-02-27 17:58:57 -0800

    Housekeeping updates

--- profile-card/src/components/UserProfile.jsx
+++ profile-card/src/components/UserProfile.jsx
@@ -5,6 +5,7 @@ export default function UserProfile({ name, email, pic }){
     return (
         <div className="profile-card">
             <h2>User Profile</h2>
+            <h3>more jsx content</h3>
             <img src={pic} alt="User Photo" className="photo"/>
             <p>Name: {name}</p>
             <p>Email: <span className="email">{email}</span></p>
